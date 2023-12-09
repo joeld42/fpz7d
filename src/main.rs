c71dce16825bfdf96d6a8370fd2c3a6789862706
Joel Davis <joeld42@gmail.com>
2023-12-09 10:04:37 -0800

    toggle debug camera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+//use std::fmt::Debug;
+
 use bevy::prelude::*;
 
 
@@ -16,18 +18,35 @@ impl Plugin for HelloPlugin {
     fn build( &self, app: &mut App ) {
         
         app.insert_resource( GreetTimer( Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .init_resource::<DebugTools>()
             .add_systems( Startup, add_people )
             .add_systems( Startup, fpz7_setup )
+            .add_systems( Update, toggle_debug_camera )
             .add_systems( Update, greet_people )
             ;
     }
 }
 
+#[derive(Component)]
+struct DebugCamera;
+
+#[derive(Resource,Default)]
+struct DebugTools {
+    dbg_cam : bool,
+}
+
+
 fn fpz7_setup (
     mut commands : Commands,
+    mut dbg: ResMut<DebugTools>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+
+    // set up debug tools    
+    println!("will setup debug tools");
+    dbg.dbg_cam = false;
+    println!("Setup debug tools done");
 
     // circular base
     commands.spawn( PbrBundle {
@@ -58,8 +77,22 @@ fn fpz7_setup (
     // Camera
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+
         ..default()
     });
+
+
+    // Debug Camera
+    commands.spawn ( (Camera3dBundle {
+        camera: Camera {
+            is_active: false,
+            ..default()
+        },
+        transform: Transform::from_xyz(0.0, 1.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),        
+        ..default()
+        },
+        DebugCamera,
+    ) );
 
 }
 
@@ -68,6 +101,31 @@ fn add_people( mut commands: Commands ) {
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn toggle_debug_camera( 
+    mut dbg: ResMut<DebugTools>,
+    //query: Query<&Camera, Option<&DebugCamera>>,
+    mut q_dbg_cam: Query<&mut Camera, With<DebugCamera> >,
+    mut q_regular_cam: Query<&mut Camera, Without<DebugCamera> >,
+    input: Res<Input<KeyCode>>,
+    ) {
+    let toggle_debug = input.just_pressed(KeyCode::Tab);
+
+    if toggle_debug {
+        
+        let use_dbg_cam: bool = !dbg.dbg_cam;
+        dbg.dbg_cam = use_dbg_cam;
+
+        for mut cam in &mut q_dbg_cam {        
+            cam.is_active = use_dbg_cam;
+        }
+        for mut cam in &mut q_regular_cam {        
+            cam.is_active = !use_dbg_cam;
+        }
+        
+    }
+
 }
 
 fn greet_people( 
@@ -80,10 +138,6 @@ fn greet_people(
             println!("hello {}!", name.0 );
         }
     }
-}
-
-fn hello_world() {
-    println!("hello world...");
 }
 
 fn main() {
