9e9ea3181626640d6af9c74bd23949d35de319f0
Joel Davis <joeld42@gmail.com>
2023-12-10 11:17:05 -0800

    bssic mouselook

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 //use std::fmt::Debug;
 
-use bevy::prelude::*;
+use bevy::{
+    prelude::*, 
+    window::CursorGrabMode,
+    app::{AppExit, ScheduleRunnerPlugin},
+    input::mouse::MouseMotion,
+};
+
+
 
 
 #[derive(Component)]
@@ -19,10 +26,13 @@ impl Plugin for HelloPlugin {
         
         app.insert_resource( GreetTimer( Timer::from_seconds(2.0, TimerMode::Repeating)))
             .init_resource::<DebugTools>()
+            .init_resource::<GameState>()
             .add_systems( Startup, add_people )
             .add_systems( Startup, fpz7_setup )
             .add_systems( Update, toggle_debug_camera )
             .add_systems( Update, greet_people )
+            .add_systems( Update, player_controller )
+            .add_systems( Update, mouse_look )
             ;
     }
 }
@@ -35,18 +45,29 @@ struct DebugTools {
     dbg_cam : bool,
 }
 
+#[derive(Resource,Default)]
+struct GameState {
+    ent_player: Option<Entity>,
+    ent_fps_camera: Option<Entity>,
+}
+
 
 fn fpz7_setup (
     mut commands : Commands,
+    mut windows: Query<&mut Window>,
     mut dbg: ResMut<DebugTools>,
+    mut game: ResMut<GameState>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
 
     // set up debug tools    
-    println!("will setup debug tools");
     dbg.dbg_cam = false;
-    println!("Setup debug tools done");
+
+    // grab mouse
+    let mut window = windows.single_mut();
+    window.cursor.visible = false;
+    window.cursor.grab_mode = CursorGrabMode::Locked;
 
     // circular base
     commands.spawn( PbrBundle {
@@ -56,13 +77,28 @@ fn fpz7_setup (
         ..default()
     });
 
+    // gamestate and player
+    game.ent_player = Some(
+        // commands.spawn(
+        //         Transform::from_xyz( 0.0, 0.0, 0.0)
+        //         ).id() );
+        commands.spawn(
+            PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+                material: materials.add(Color::rgb_u8(250, 60, 60).into()),
+                transform: Transform::from_xyz(0.0, 0.0, 0.0),
+                ..default()
+            }).id() );
+                
+
     // cube
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
         material: materials.add(Color::rgb_u8(124, 144, 255).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        transform: Transform::from_xyz(1.0, 0.5, 2.0),
         ..default()
     });
+
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -75,11 +111,11 @@ fn fpz7_setup (
     });
 
     // Camera
-    commands.spawn(Camera3dBundle {
+    game.ent_fps_camera = Some( commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
 
         ..default()
-    });
+    }).id() );
 
 
     // Debug Camera
@@ -104,11 +140,13 @@ fn add_people( mut commands: Commands ) {
 }
 
 fn toggle_debug_camera( 
+    mut windows: Query<&mut Window>,
     mut dbg: ResMut<DebugTools>,
     //query: Query<&Camera, Option<&DebugCamera>>,
     mut q_dbg_cam: Query<&mut Camera, With<DebugCamera> >,
     mut q_regular_cam: Query<&mut Camera, Without<DebugCamera> >,
     input: Res<Input<KeyCode>>,
+    mut app_exit_events: EventWriter<AppExit>,
     ) {
     let toggle_debug = input.just_pressed(KeyCode::Tab);
 
@@ -123,9 +161,115 @@ fn toggle_debug_camera(
         for mut cam in &mut q_regular_cam {        
             cam.is_active = !use_dbg_cam;
         }
+
+        // mouse grab
+        let mut window = windows.single_mut();
+        if use_dbg_cam {
+            //mouse grab off in debug mode
+            window.cursor.visible = true;
+            window.cursor.grab_mode = CursorGrabMode::None;
+        } else {
+            window.cursor.visible = false;
+            window.cursor.grab_mode = CursorGrabMode::Locked;
+        }
         
     }
 
+    // Check for app exit
+    if (input.just_pressed( KeyCode::Escape )) {        
+        app_exit_events.send(AppExit);
+    }
+
+}
+
+fn player_controller(
+    time: Res<Time>,
+    game: Res<GameState>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut transforms: Query<&mut Transform>,
+    ) 
+    {
+        let move_speed = 10f32;
+        let mut move_x : f32 = 0.0;
+        let mut move_y : f32 = 0.0;
+        
+        if keyboard_input.pressed( KeyCode::W ) ||
+            keyboard_input.pressed( KeyCode::Up ) {
+                move_y = move_y + move_speed;
+            }
+
+        if keyboard_input.pressed( KeyCode::S ) ||
+            keyboard_input.pressed( KeyCode::Down ) {
+                move_y = move_y - move_speed;
+            }
+
+        if keyboard_input.pressed( KeyCode::A ) ||
+            keyboard_input.pressed( KeyCode::Left ) {
+                move_x = move_x - move_speed;
+            }
+
+        if keyboard_input.pressed( KeyCode::D ) ||
+            keyboard_input.pressed( KeyCode::Right ) {
+                move_x = move_x + move_speed;
+            }
+
+        let move_dir_fwd = Vec3::Z;
+        let move_dir_right = Vec3::X;
+
+        let curr = transforms.get( game.ent_player.unwrap() ).unwrap().clone();
+        
+
+        let upd_pos = curr.translation + 
+            (move_dir_fwd * move_y * time.delta_seconds() ) +
+            (move_dir_right * move_x * time.delta_seconds());
+        
+        // TODO: check for collision        
+        
+        // let mut player_transform = transforms.get_mut(game.ent_player.unwrap()).unwrap();
+        // player_transform.translation = upd_pos;
+        // player_transform.rotation = curr.rotation;
+        
+
+        let mut player_transform = transforms.get_mut(game.ent_player.unwrap()).unwrap();
+        player_transform.translation = upd_pos;
+        player_transform.rotation = curr.rotation;
+        
+        let mut fps_camera_transform = transforms.get_mut(game.ent_fps_camera.unwrap()).unwrap();
+        let fps_cam_offset = Vec3 { x : 0.0, y : 1.5, z : 0.0 };
+        fps_camera_transform.translation = upd_pos + fps_cam_offset;
+        //fps_camera_transform.rotation = curr.rotation;
+
+
+
+    }
+
+fn mouse_look (
+    time: Res<Time>,
+    game: Res<GameState>,
+    mut motion_evr: EventReader<MouseMotion>,
+    mut transforms: Query<&mut Transform>,
+) {
+    let lookstr = time.delta_seconds();
+    let move_ang = 4.0 * (3.1416 / 180.0 );
+
+    for ev in motion_evr.iter() {
+        println!("Mouse moved: X: {} px, Y: {} px", ev.delta.x, ev.delta.y);
+
+        let rot_x = -ev.delta.x * lookstr;
+        let rot_y = -ev.delta.y * lookstr;
+
+        //let curr = transforms.get( game.ent_fps_camera.unwrap() ).unwrap().clone();
+
+        let mut fps_camera_transform = transforms.get_mut(game.ent_fps_camera.unwrap()).unwrap();
+
+        let xrot = Quat::from_axis_angle( Vec3::Y,  rot_x * move_ang );
+        let yrot = Quat::from_axis_angle( Vec3::X,  rot_y * move_ang );
+
+        fps_camera_transform.rotation =  xrot * fps_camera_transform.rotation;
+        fps_camera_transform.rotation =  fps_camera_transform.rotation * yrot;
+        
+
+    }
 }
 
 fn greet_people( 
